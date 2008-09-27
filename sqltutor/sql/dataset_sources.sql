179f2e7b056f8408faccdd79fbaa97b1bd01c26d
cepek <cepek>
2008-09-27 19:09:13 +0000

    *** empty log message ***

--- sqltutor/sql/dataset_sources.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sqltutor.dataset_sources (
-  dataset   VARCHAR(12),
-  year      INT,
-  sources   VARCHAR(120)
-);
-
--- sqltutor/sql/datasets.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE sqltutor.datasets (
-  dataset   VARCHAR(12),
-  ord       INT,
-  ds_table  VARCHAR(20),
-  columns   VARCHAR(65)
-);
-
