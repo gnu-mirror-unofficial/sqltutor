4ede6ed06cb6eb47cbf0c5930fe86b98dc5b7f2c
Ales Cepek <cepek@gnu.org>
2011-06-26 19:38:35 +0200

    db schema image helper file

--- doc/schema/text.sh
+++ doc/schema/text.sh
@@ -1,6 +1,15 @@
+# g++ -o premp premp.cpp
+# ./premp < premp.schema > schema.mp
+# mpost schema.mp
+# mv schema.1 schema.eps
+# pdflatex demo.tex
+# mv demo.pdf schema.pdf
+# convert -density 90 schema.pdf schema.png
+# ./text.sh > schema.txt
+#
 grep ^\ *\#.*[abc], premp.schema | sed s/[abc],// | sed s/#//g | \
     sed s/,/,\ /g | \
-    awk '{print "      " $1 " (" $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15")"}' | \
+    awk '{print "      " $1 " (" $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16")"}' | \
     sed s/\;// | sed s/,/,\ /g
 
 
